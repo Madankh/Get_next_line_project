ba032ca021cbec3ee0a2de0e3dc98da8658560a5
madankh <khadkasuman0000@gmail.com>
2024-05-18 18:33:23 +0545

    main

--- main.c
+++ main.c
@@ -15,8 +15,14 @@
 
 int main(){
     int fd;
+    char buf[256];
+    int chars_read;
     fd = open("file.txt" , O_RDWR | O_CREAT);
-    // printf("fd of file", "%d\n", fd);
-    write(1,"hello", 5);
-    write(fd, "Hello", 5);
+    while ((chars_read = read(fd, buf, 5)))
+    {
+        buf[chars_read] = '\0';
+        printf("buf-> %s \n", buf);
+    }
+    
+    
 }
