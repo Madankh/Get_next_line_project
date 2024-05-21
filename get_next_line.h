dce59121c9489a6101f7f1f94c58a875c88c1cad
madankh <khadkasuman0000@gmail.com>
2024-05-21 17:38:46 +0545

    random

---
+++ get_next_line.h
@@ -0,0 +1,12 @@
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 10
+#endif
+
+#endif // GET_NEXT_LINE_H
+
+#include<fcntl.h>
+#include<stdlib.h>
+#include<unistd.h>
